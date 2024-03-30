633ce450f115cc3049fb1e1e6446e2420864e068
ChessLandsknecht <jl.alexh@gmail.com>
2024-03-30 20:34:57 +1100

    Fun and Engaging Gameplay u suck

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        for (int k = 0; k < 10; k++)
+        {
+            for (int l = 0; l < 10; l++)
+            {
+                Console.WriteLine(i*j*k*l);
+            }
+            
+        }
+        
+    }
+    
+}
+
+
